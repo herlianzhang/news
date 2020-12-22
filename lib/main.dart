2ec90a2af7a7873f772d12fe8d98dee6a3ca918d
herlianzhang <herlianzhang370@gmail.com>
2020-12-22 13:50:53 +0700

    test 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,3 @@
 import 'package:flutter/material.dart';
 import 'src/app.dart';
 void main() => runApp(App());
-
-class  extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-            
-      ),
-    );
-  }
-}
---
+++ lib/src/screens/news_list.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:news/src/blocs/stories_bloc.dart';
+
+class NewsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final bloc = BlocProvider.of<StoriesBloc>(context);
+
+    bloc.fetchTopIds();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Top News'),
+      ),
+      body: buildList(bloc),
+    );
+  }
+
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder<List<int>>(
+      stream: bloc.topIds,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(child: CircularProgressIndicator(),);
+        }
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, index) {
+            return Text('${snapshot.data[index]}');
+          },
+        );
+      },
+    );
+  }
+}
